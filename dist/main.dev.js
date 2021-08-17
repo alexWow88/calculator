ab9b8bc790a1461f1c86f902acee1763c63937b7
Alex Wower <woweralexander@gmail.com>
2021-08-17 14:12:07 +0100

    additional display field created

---
+++ dist/main.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.getElementById("button-1").onclick = function () {
+  oneFn();
+};
+
+function oneFn() {
+  document.getElementById("display").innerHTML = "2S";
+}
+
+;
---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("button-1").onclick = function() {oneFn()};
+
+function oneFn() {
+  document.getElementById("display").innerHTML = "2S";
+};
