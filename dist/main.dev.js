cb88bd22963df10d40bcf21fa435aec607236e8f
Alex Wower <woweralexander@gmail.com>
2021-08-17 17:27:02 +0100

    numb buttons adding to array. display at once iss

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,11 +1,51 @@
 "use strict";
 
-document.getElementById("button-1").onclick = function () {
-  oneFn();
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+var displayList = document.querySelector(".display-list");
+var displayResult = document.querySelector(".display-result");
+var buttonOne = document.getElementById("button-1");
+var buttonTwo = document.getElementById("button-2");
+var buttonThree = document.getElementById("button-3");
+var buttonFour = document.getElementById("button-4");
+var buttonFive = document.getElementById("button-5");
+var buttonSix = document.getElementById("button-6");
+var buttonSeven = document.getElementById("button-7");
+var buttonEight = document.getElementById("button-8");
+var buttonNine = document.getElementById("button-9");
+var buttonZero = document.getElementById("button-0");
+var buttonDot = document.getElementById("button-dot");
+var buttonEquals = document.getElementById("button-equals");
+var buttonPlus = document.getElementById("button-plus");
+var buttonMinus = document.getElementById("button-minus");
+var buttonTimes = document.getElementById("button-times");
+var buttonDivide = document.getElementById("button-divide");
+var buttonNegPos = document.getElementById("button-negPos");
+var buttonClear = document.getElementById("button-clear");
+var buttonPercent = document.getElementById("button-percent");
+var resultOfArr = [];
+var joinOfArr = [];
+var calcNum = [];
+
+var attachNum = function attachNum(calcNum) {
+  resultOfArr.push.apply(resultOfArr, _toConsumableArray(calcNum));
+  displayResult.innerHTML = resultOfArr;
+  displayList.innerHTML = resultOfArr.join("");
 };
 
-function oneFn() {
-  document.getElementById("display").innerHTML = "2S";
-}
-
-;
+buttonOne.addEventListener("click", attachNum("1"));
+buttonTwo.addEventListener("click", attachNum("2"));
+buttonThree.addEventListener("click", attachNum("3"));
+buttonFour.addEventListener("click", attachNum("4"));
+buttonFive.addEventListener("click", attachNum("5"));
+buttonSix.addEventListener("click", attachNum("6"));
+buttonSeven.addEventListener("click", attachNum("7"));
+buttonEight.addEventListener("click", attachNum("8"));
+buttonNine.addEventListener("click", attachNum("9"));
+buttonZero.addEventListener("click", attachNum("0"));
--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-document.getElementById("button-1").onclick = function() {oneFn()};
+let displayList = document.querySelector(".display-list");
+let displayResult = document.querySelector(".display-result");
+let buttonOne = document.getElementById("button-1");
+let buttonTwo = document.getElementById("button-2");
+let buttonThree = document.getElementById("button-3");
+let buttonFour = document.getElementById("button-4");
+let buttonFive = document.getElementById("button-5");
+let buttonSix = document.getElementById("button-6");
+let buttonSeven = document.getElementById("button-7");
+let buttonEight = document.getElementById("button-8");
+let buttonNine = document.getElementById("button-9");
+let buttonZero = document.getElementById("button-0");
+let buttonDot = document.getElementById("button-dot");
+let buttonEquals = document.getElementById("button-equals");
+let buttonPlus = document.getElementById("button-plus");
+let buttonMinus = document.getElementById("button-minus");
+let buttonTimes = document.getElementById("button-times");
+let buttonDivide = document.getElementById("button-divide");
+let buttonNegPos = document.getElementById("button-negPos");
+let buttonClear = document.getElementById("button-clear");
+let buttonPercent = document.getElementById("button-percent");
 
-function oneFn() {
-  document.getElementById("display").innerHTML = "2S";
+let resultOfArr = [];
+let joinOfArr = [];
+let calcNum = [];
+
+const attachNum = (calcNum) => {
+  resultOfArr.push(...calcNum);
+  displayResult.innerHTML = resultOfArr;
+  displayList.innerHTML = resultOfArr.join("");
 };
+
+buttonOne.addEventListener("click", attachNum("1"));
+buttonTwo.addEventListener("click", attachNum("2"));
+buttonThree.addEventListener("click", attachNum("3"));
+buttonFour.addEventListener("click", attachNum("4"));
+buttonFive.addEventListener("click", attachNum("5"));
+buttonSix.addEventListener("click", attachNum("6"));
+buttonSeven.addEventListener("click", attachNum("7"));
+buttonEight.addEventListener("click", attachNum("8"));
+buttonNine.addEventListener("click", attachNum("9"));
+buttonZero.addEventListener("click", attachNum("0"));
