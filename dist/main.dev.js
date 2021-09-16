ed8799de003fa9659e71d8cbd792bb40f0e1756e
Alex Wower <woweralexander@gmail.com>
2021-09-16 17:58:05 +0100

    2 display List/Result func added - shorten code

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -54,22 +54,37 @@ var attachDot = function attachDot(newNum) {
   displayList.innerHTML = resultOfArr.join("");
 };
 
+var displayResultAndList = function displayResultAndList() {
+  var _listOfArr3;
+
+  displayResult.innerHTML = resultOfArr;
+  resultOfArr = String(resultOfArr);
+  listOfArr = [];
+
+  (_listOfArr3 = listOfArr).push.apply(_listOfArr3, _toConsumableArray(resultOfArr));
+
+  resultOfArr = resultOfArr.split("");
+  displayList.innerHTML = listOfArr.join("");
+};
+
+var displayResultAndSplitResultsArr = function displayResultAndSplitResultsArr() {
+  displayResult.innerHTML = resultOfArr;
+  resultOfArr = String(resultOfArr);
+  resultOfArr = resultOfArr.split("");
+};
+
 var attachSymb = function attachSymb(newSymb) {
-  var _resultOfArr3, _listOfArr3;
+  var _resultOfArr3, _listOfArr4;
 
   if (resultOfArr.join("").includes("-")) {
     var splitResultArr = resultOfArr.join("").split("-");
 
     if (resultOfArr[0] == "-" && splitResultArr.length === 3) {
       resultOfArr = -1 * parseFloat(splitResultArr[1]) - parseFloat(splitResultArr[2]);
-      displayResult.innerHTML = resultOfArr;
-      resultOfArr = String(resultOfArr);
-      resultOfArr = resultOfArr.split("");
+      displayResultAndSplitResultsArr();
     } else if (resultOfArr[0] !== "-" && splitResultArr.length == 2) {
       resultOfArr = parseFloat(splitResultArr[0]) - parseFloat(splitResultArr[1]);
-      displayResult.innerHTML = resultOfArr;
-      resultOfArr = String(resultOfArr);
-      resultOfArr = resultOfArr.split("");
+      displayResultAndSplitResultsArr();
     }
 
     ;
@@ -81,28 +96,22 @@ var attachSymb = function attachSymb(newSymb) {
     var _splitResultArr = resultOfArr.join("").split("+");
 
     resultOfArr = parseFloat(_splitResultArr[0]) + parseFloat(_splitResultArr[1]);
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    resultOfArr = resultOfArr.split("");
+    displayResultAndSplitResultsArr();
   } else if (resultOfArr.join("").includes("*")) {
     var _splitResultArr2 = resultOfArr.join("").split("*");
 
     resultOfArr = parseFloat(_splitResultArr2[0]) * parseFloat(_splitResultArr2[1]);
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    resultOfArr = resultOfArr.split("");
+    displayResultAndSplitResultsArr();
   } else if (resultOfArr.join("").includes("/")) {
     var _splitResultArr3 = resultOfArr.join("").split("/");
 
     resultOfArr = parseFloat(_splitResultArr3[0]) / parseFloat(_splitResultArr3[1]);
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    resultOfArr = resultOfArr.split("");
+    displayResultAndSplitResultsArr();
   }
 
   (_resultOfArr3 = resultOfArr).push.apply(_resultOfArr3, _toConsumableArray(newSymb));
 
-  (_listOfArr3 = listOfArr).push.apply(_listOfArr3, _toConsumableArray(newSymb));
+  (_listOfArr4 = listOfArr).push.apply(_listOfArr4, _toConsumableArray(newSymb));
 
   displayList.innerHTML = listOfArr.join("");
 };
@@ -116,169 +125,61 @@ var clearDisplay = function clearDisplay() {
 
 var negPosResult = function negPosResult() {
   if (resultOfArr.join("").includes("+")) {
-    var _listOfArr4;
-
     var splitResultArr = resultOfArr.join("").split("+");
     resultOfArr = (parseFloat(splitResultArr[0]) + parseFloat(splitResultArr[1])) * -1;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr4 = listOfArr).push.apply(_listOfArr4, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else if (resultOfArr.join("").includes("*")) {
-    var _listOfArr5;
-
     var _splitResultArr4 = resultOfArr.join("").split("*");
 
     resultOfArr = parseFloat(_splitResultArr4[0]) * parseFloat(_splitResultArr4[1]) * -1;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr5 = listOfArr).push.apply(_listOfArr5, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else if (resultOfArr.join("").includes("/")) {
-    var _listOfArr6;
-
     var _splitResultArr5 = resultOfArr.join("").split("/");
 
     resultOfArr = parseFloat(_splitResultArr5[0]) / parseFloat(_splitResultArr5[1]) * -1;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr6 = listOfArr).push.apply(_listOfArr6, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else if (resultOfArr.join("").includes("-")) {
     var _splitResultArr6 = resultOfArr.join("").split("-");
 
     if (_splitResultArr6[0] == "" && _splitResultArr6.length === 2 && !resultOfArr.join("").includes("+") && !resultOfArr.join("").includes("*") && !resultOfArr.join("").includes("/")) {
-      var _listOfArr7;
-
       resultOfArr = parseFloat(_splitResultArr6[1]);
-      displayResult.innerHTML = resultOfArr;
-      resultOfArr = String(resultOfArr);
-      listOfArr = [];
-
-      (_listOfArr7 = listOfArr).push.apply(_listOfArr7, _toConsumableArray(resultOfArr));
-
-      resultOfArr = resultOfArr.split("");
-      displayList.innerHTML = listOfArr.join("");
+      displayResultAndList();
     } else if (_splitResultArr6[0] == "" && _splitResultArr6.length === 3) {
-      var _listOfArr8;
-
       resultOfArr = (-1 * parseFloat(_splitResultArr6[1]) - parseFloat(_splitResultArr6[2])) * -1;
-      displayResult.innerHTML = resultOfArr;
-      resultOfArr = String(resultOfArr);
-      listOfArr = [];
-
-      (_listOfArr8 = listOfArr).push.apply(_listOfArr8, _toConsumableArray(resultOfArr));
-
-      resultOfArr = resultOfArr.split("");
-      displayList.innerHTML = listOfArr.join("");
+      displayResultAndList();
     } else if (_splitResultArr6[0] !== "") {
-      var _listOfArr9;
-
       resultOfArr = (parseFloat(_splitResultArr6[0]) - parseFloat(_splitResultArr6[1])) * -1;
-      displayResult.innerHTML = resultOfArr;
-      resultOfArr = String(resultOfArr);
-      listOfArr = [];
-
-      (_listOfArr9 = listOfArr).push.apply(_listOfArr9, _toConsumableArray(resultOfArr));
-
-      resultOfArr = resultOfArr.split("");
-      displayList.innerHTML = listOfArr.join("");
+      displayResultAndList();
     }
   } else {
-    var _listOfArr10;
-
     resultOfArr = parseFloat(resultOfArr.join("")) * -1;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr10 = listOfArr).push.apply(_listOfArr10, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   }
 };
 
 var percentResult = function percentResult() {
   if (resultOfArr.join("").includes("+")) {
-    var _listOfArr11;
-
     var splitResultArr = resultOfArr.join("").split("+");
     resultOfArr = (parseFloat(splitResultArr[0]) + parseFloat(splitResultArr[1])) * 100;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr11 = listOfArr).push.apply(_listOfArr11, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else if (resultOfArr.join("").includes("-")) {
-    var _listOfArr12;
-
     var _splitResultArr7 = resultOfArr.join("").split("-");
 
     resultOfArr = (parseFloat(_splitResultArr7[0]) - parseFloat(_splitResultArr7[1])) * 100;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr12 = listOfArr).push.apply(_listOfArr12, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else if (resultOfArr.join("").includes("*")) {
-    var _listOfArr13;
-
     var _splitResultArr8 = resultOfArr.join("").split("*");
 
     resultOfArr = parseFloat(_splitResultArr8[0]) * parseFloat(_splitResultArr8[1]) * 100;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr13 = listOfArr).push.apply(_listOfArr13, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else if (resultOfArr.join("").includes("/")) {
-    var _listOfArr14;
-
     var _splitResultArr9 = resultOfArr.join("").split("/");
 
     resultOfArr = parseFloat(_splitResultArr9[0]) / parseFloat(_splitResultArr9[1]) * 100;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr14 = listOfArr).push.apply(_listOfArr14, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else {
-    var _listOfArr15;
-
     resultOfArr = parseFloat(resultOfArr.join("")) * 100;
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr15 = listOfArr).push.apply(_listOfArr15, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   }
 };
 
@@ -287,29 +188,11 @@ var calcResult = function calcResult() {
     var splitResultArr = resultOfArr.join("").split("-");
 
     if (resultOfArr[0] == "-" && splitResultArr.length === 3) {
-      var _listOfArr16;
-
       resultOfArr = -1 * parseFloat(splitResultArr[1]) - parseFloat(splitResultArr[2]);
-      displayResult.innerHTML = resultOfArr;
-      resultOfArr = String(resultOfArr);
-      listOfArr = [];
-
-      (_listOfArr16 = listOfArr).push.apply(_listOfArr16, _toConsumableArray(resultOfArr));
-
-      displayList.innerHTML = listOfArr.join("");
-      resultOfArr = resultOfArr.split("");
+      displayResultAndList();
     } else if (resultOfArr[0] !== "-" && splitResultArr.length == 2) {
-      var _listOfArr17;
-
       resultOfArr = parseFloat(splitResultArr[0]) - parseFloat(splitResultArr[1]);
-      displayResult.innerHTML = resultOfArr;
-      resultOfArr = String(resultOfArr);
-      listOfArr = [];
-
-      (_listOfArr17 = listOfArr).push.apply(_listOfArr17, _toConsumableArray(resultOfArr));
-
-      displayList.innerHTML = listOfArr.join("");
-      resultOfArr = resultOfArr.split("");
+      displayResultAndList();
     }
 
     ;
@@ -318,47 +201,20 @@ var calcResult = function calcResult() {
   ;
 
   if (resultOfArr.join("").includes("+")) {
-    var _listOfArr18;
-
     var _splitResultArr10 = resultOfArr.join("").split("+");
 
     resultOfArr = parseFloat(_splitResultArr10[0]) + parseFloat(_splitResultArr10[1]);
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr18 = listOfArr).push.apply(_listOfArr18, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else if (resultOfArr.join("").includes("*")) {
-    var _listOfArr19;
-
     var _splitResultArr11 = resultOfArr.join("").split("*");
 
     resultOfArr = parseFloat(_splitResultArr11[0]) * parseFloat(_splitResultArr11[1]);
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr19 = listOfArr).push.apply(_listOfArr19, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   } else if (resultOfArr.join("").includes("/")) {
-    var _listOfArr20;
-
     var _splitResultArr12 = resultOfArr.join("").split("/");
 
     resultOfArr = parseFloat(_splitResultArr12[0]) / parseFloat(_splitResultArr12[1]);
-    displayResult.innerHTML = resultOfArr;
-    resultOfArr = String(resultOfArr);
-    listOfArr = [];
-
-    (_listOfArr20 = listOfArr).push.apply(_listOfArr20, _toConsumableArray(resultOfArr));
-
-    resultOfArr = resultOfArr.split("");
-    displayList.innerHTML = listOfArr.join("");
+    displayResultAndList();
   }
 };
 
